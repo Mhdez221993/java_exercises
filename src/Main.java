692c4b4be3b002e1a0311a4f0c808c63d99180d3
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-15 13:17:53 +0200

    Types used to work with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int number = Integer.MAX_VALUE + 1;
+        int number = Integer.MAX_VALUE;
+
+        long bigNumber = Long.MAX_VALUE;
+
+        float floatNumber = 23.2F;
+
+        double decimalNumber = 23.3234;
+
+        byte byteNumber = -128;
+
+        short shortNumber = 32767;
+
         System.out.println(number);
     }
 }
