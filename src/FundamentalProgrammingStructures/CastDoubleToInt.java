81f016740bc06f3952409788c7a15c6da731c64f
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-22 15:19:16 +0200

    Cast double to int

---
+++ src/FundamentalProgrammingStructures/CastDoubleToInt.java
@@ -0,0 +1,11 @@
+package FundamentalProgrammingStructures;
+
+public class CastDoubleToInt {
+    public static void main(String[] args) {
+        double n = Double.MAX_VALUE;
+
+        int nToInt = (int) n;
+
+        System.out.println(nToInt);
+    }
+}
