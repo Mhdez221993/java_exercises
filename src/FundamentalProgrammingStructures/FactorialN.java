3e6b69de1eaa611bbc3604ffb66f4994f00d98ba
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-23 13:14:02 +0200

    Refactor BigInteger operation

--- src/FundamentalProgrammingStructures/FactorialN.java
+++ src/FundamentalProgrammingStructures/FactorialN.java
@@ -5,8 +5,8 @@ import java.math.BigInteger;
 public class FactorialN {
     public static void main(String[] args) {
         BigInteger product = BigInteger.ONE;
-        for (int n = 1; n <= 1000; n++) {
-            product = BigInteger.valueOf(n).multiply(product);
+        for (int n = 1; n <= 5; n++) {
+            product = product.multiply(BigInteger.valueOf(n));
         }
         System.out.println(product);
     }
