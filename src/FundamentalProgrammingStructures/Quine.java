9970695807ecdd0354b36c762480ad002485d837
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-26 00:50:15 +0200

    Update Quine with text block

--- src/FundamentalProgrammingStructures/Quine.java
+++ src/FundamentalProgrammingStructures/Quine.java
@@ -8,8 +8,25 @@ own source code. Write such a program in Java, using a text block.
 public class Quine {
     public static void main(String[] args) {
         char c=34;
-        System.out.println(s+c+s+c+';'+'}');
+        System.out.println(s+c+s);
     }
-    static String s="public class Quine { public static void main(String[] args) { char c=34; System.out.println(s+c+s+c+';'+'}'); } static String s=";
+    static String s = """
+            public class Quine {
+                 public static void main(String[] args) {
+                     char c=34;
+                     System.out.println(s+c+s);
+                 }
+                 static String s = ""\"
+                         public class Quine {
+                             public static void main(String[] args) {
+                                 char c=34;
+                                 System.out.println(s+c+s);
+                             }
+                             static String s="public class Quine { public static void main(String[] args) { char c=34; System.out.println(s+c+s+c+';'+'}'); } static String s=";
+                         }
+                         ""\";
+             }
+             
+            """;
 }
 
---
+++ src/FundamentalProgrammingStructures/QuineReadingFile.java
@@ -0,0 +1,9 @@
+package FundamentalProgrammingStructures;
+
+/*
+A “Quine” is a program that, prints its
+own source code. Write such a program in Java, using a text block.
+ */
+
+public class QuineReadingFile {
+}
