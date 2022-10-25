9f3b68efc123f0da9f5b248c8a5d60995a8209a2
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-25 09:44:24 +0200

    Find and print the nonASCII value

---
+++ src/FundamentalProgrammingStructures/NonASCIIValues.java
@@ -0,0 +1,21 @@
+package FundamentalProgrammingStructures;
+
+import java.util.Scanner;
+
+/*
+Write a program that reads a line of text and prints all characters that are
+not ASCII, together with their Unicode values.
+ */
+
+public class NonASCIIValues {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Read text line
+        // iterate
+        // if the code point is gratter than 255 it is not ASSCi
+        // print that value's unicode
+
+        System.out.println(ascci);
+    }
+}
