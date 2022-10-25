4e75548a849660ad0cff7472d18d1283e7f862d4
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-26 01:00:34 +0200

    Create Quine that reads from file

--- src/FundamentalProgrammingStructures/QuineReadingFile.java
+++ src/FundamentalProgrammingStructures/QuineReadingFile.java
@@ -5,5 +5,17 @@ A “Quine” is a program that, prints its
 own source code. Write such a program in Java, using a text block.
  */
 
+import java.io.*;
+
 public class QuineReadingFile {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("QuineReadingFile.java")));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
+    }
 }
+
+// Program no finished yet
