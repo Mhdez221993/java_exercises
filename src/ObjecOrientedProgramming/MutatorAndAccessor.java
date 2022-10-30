f48c180f68369eb23193e57f20d49575d5414446
Moises Hernandez <mhdezcoronado@gmail.com>
2022-10-30 12:48:26 +0100

    Write example of string literal & with the new keyword

---
+++ src/ObjecOrientedProgramming/MutatorAndAccessor.java
@@ -0,0 +1,11 @@
+package ObjecOrientedProgramming;
+
+/*
+Can you ever have a mutator method return something other than void?
+Can you ever have an accessor method return void? Give examples
+when possible.
+ */
+
+public class MutatorAndAccessor {
+
+}
