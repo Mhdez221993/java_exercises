16bf3407b351962aa84059ce4a074e63ec50b898
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-01 18:17:12 +0100

    Improve leap year program

--- src/FundamentalProgrammingStructures/LeapYear.java
+++ src/FundamentalProgrammingStructures/LeapYear.java
@@ -2,9 +2,15 @@ package FundamentalProgrammingStructures;
 
 // A year is leap, if it is divisible by 4 and 400. But, not by 100
 
+import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 20220;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Java program to verify if it is a leap year, please introduce a years");
+
+        int year = in.nextInt();
 
         if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             System.out.println("Leap Year");
