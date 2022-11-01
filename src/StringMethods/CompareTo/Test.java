0e0c0bbd8b03db932171fdaf5d18943862216091
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-01 09:22:07 +0100

    Cast a byte with plus asigment

---
+++ src/StringMethods/CompareTo/Test.java
@@ -0,0 +1,10 @@
+package StringMethods.CompareTo;
+
+public class Test {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+        a += b;
+        System.out.println(a);
+    }
+}
