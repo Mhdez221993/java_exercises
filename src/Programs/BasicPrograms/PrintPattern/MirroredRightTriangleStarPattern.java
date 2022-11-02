58517f00cf49b3127a00f4f93ff2855d1a7706f8
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-02 23:26:23 +0100

    Write a program that prints a mirrored right triangle start pattern

---
+++ src/Programs/BasicPrograms/PrintPattern/MirroredRightTriangleStarPattern.java
@@ -0,0 +1,29 @@
+package Programs.BasicPrograms.PrintPattern;
+
+import java.util.Scanner;
+
+public class MirroredRightTriangleStarPattern {
+    static void printPatter(String pattern, int rows) {
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 1; j <= (rows - i); j++)
+                System.out.print(" ");
+
+            for (int j = 0; j <= i; j++)
+                System.out.print(pattern);
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows you want to print: ");
+
+        int rows = in.nextInt();
+
+        printPatter("*", rows);
+    }
+}
