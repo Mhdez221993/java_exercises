c08458b63c79fb46c2643f5920e8ac657760d959
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-02 20:14:38 +0100

    Write a program to verify if a number is armstrong

--- src/Programs/BasicPrograms/ArmstrongNumber.java
+++ src/Programs/BasicPrograms/ArmstrongNumber.java
@@ -1,21 +1,54 @@
 package Programs.BasicPrograms;
 
+import java.util.Scanner;
+
 /*
 An Armstrong number is a positive m-digit number that is equal to the sum of the mth powers of their digits.
 It is also known as pluperfect, or Plus Perfect, or Narcissistic number. It is an OEIS sequence A005188.
 Let’s understand it through an example.
 
 Armstrong Number Example
-1: 11 = 1
+1: 1^1 = 1
 
-2: 21 = 2
+2: 2^1 = 2
 
-153: 13 + 53 + 33 = 1 + 125+ 27 = 153
+153: 1^3 + 5^3 + 3^3 = 1 + 125+ 27 = 153
 
-125: 13 + 23 + 53 = 1 + 8 + 125 = 134 (Not an Armstrong Number)
+125: 1^3 + 2^3 + 5^3 = 1 + 8 + 125 = 134 (Not an Armstrong Number)
  */
 public class ArmstrongNumber {
-    public static void main(String[] args) {
+    static boolean isArmstrong(int n) {
+        int digits = 0;
+        int temp = n;
 
+        while (temp > 0) {
+            temp /= 10;
+            digits++;
+        }
+
+        temp = n;
+        int sum = 0;
+        int remainder = 0;
+
+        while (temp > 0) {
+            remainder = temp % 10;
+            sum += (Math.pow(remainder, digits));
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the limit: ");
+
+        int n = in.nextInt();
+
+        System.out.println("Armstrong Numbers up to "+ n + " are:");
+
+        for (int i = 0; i < n; i++)
+            if (isArmstrong(i))
+                System.out.print(i+" ");
     }
 }
