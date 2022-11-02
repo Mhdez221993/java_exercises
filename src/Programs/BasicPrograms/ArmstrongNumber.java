f95a19b89c4d32e7867f59be10ce34108d5289e2
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-02 09:36:33 +0100

    Define armstrong number

---
+++ src/Programs/BasicPrograms/ArmstrongNumber.java
@@ -0,0 +1,21 @@
+package Programs.BasicPrograms;
+
+/*
+An Armstrong number is a positive m-digit number that is equal to the sum of the mth powers of their digits.
+It is also known as pluperfect, or Plus Perfect, or Narcissistic number. It is an OEIS sequence A005188.
+Let’s understand it through an example.
+
+Armstrong Number Example
+1: 11 = 1
+
+2: 21 = 2
+
+153: 13 + 53 + 33 = 1 + 125+ 27 = 153
+
+125: 13 + 23 + 53 = 1 + 8 + 125 = 134 (Not an Armstrong Number)
+ */
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+
+    }
+}
