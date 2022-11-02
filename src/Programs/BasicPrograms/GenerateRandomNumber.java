ae13c90055160dccc6513938d2f351c02b2a0aa3
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-02 20:40:24 +0100

    Generate a random number using Math.random()

--- src/Programs/BasicPrograms/GenerateRandomNumber.java
+++ src/Programs/BasicPrograms/GenerateRandomNumber.java
@@ -12,6 +12,8 @@ public class GenerateRandomNumber {
     public static void main(String[] args) {
         int min = 5;
         int max = 10;
-        System.out.println("1s Random Number "+ (Math.random() * (max - min + 1) + min));
+        int random = (int)(Math.random() * (max - min + 1) + min);
+
+        System.out.println("1s Random Number "+ random);
     }
 }
