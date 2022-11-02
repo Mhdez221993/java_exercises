53927bd0c9c0f3f548b1388d8b26e663e5ce9454
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-02 23:13:56 +0100

    Write a program that prints a downward triangle start pattern

---
+++ src/Programs/BasicPrograms/PrintPattern/DownwardTriangleStarPattern.java
@@ -0,0 +1,26 @@
+package Programs.BasicPrograms.PrintPattern;
+
+import java.util.Scanner;
+
+public class DownwardTriangleStarPattern {
+    static void printPatter(String pattern, int rows) {
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < (rows - i); j++)
+                System.out.print(pattern);
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows you want to print: ");
+
+        int rows = in.nextInt();
+
+        printPatter("* ", rows);
+    }
+}
