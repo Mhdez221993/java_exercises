c4b48f95ef6a5dc3a81494e1a6e71737da8f9355
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-03 21:42:13 +0100

    Write a program that print a number pattern 5

---
+++ src/Programs/BasicPrograms/PrintPattern/NumberPattern_5.java
@@ -0,0 +1,24 @@
+package Programs.BasicPrograms.PrintPattern;
+
+import java.util.Scanner;
+
+public class NumberPattern_5 {
+    static void printNumberPattern(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++ )
+                System.out.print(i+" ");
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows you want to print: ");
+
+        int n = sc.nextInt();
+
+        printNumberPattern(n);
+    }
+}
