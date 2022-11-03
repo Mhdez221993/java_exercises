31c2c182f61bce8218e713112c74a550c817a9e0
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-03 19:45:06 +0100

    Write a program to print a trinagle start pattern

---
+++ src/Programs/BasicPrograms/PrintPattern/TriangleStarPattern.java
@@ -0,0 +1,23 @@
+package Programs.BasicPrograms.PrintPattern;
+
+public class TriangleStarPattern {
+    static void printPattern(String pattern, int n) {
+        for (int i = 1; i <= n; i++) {
+           for (int j = i; j < n; j++)
+               System.out.print(" ");
+
+            for (int j = 1; j <= (2*i-1); j++) {
+                if (j == 1 || i == n || j == (2*i-1))
+                    System.out.print(pattern);
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printPattern("*", 9);
+    }
+}
