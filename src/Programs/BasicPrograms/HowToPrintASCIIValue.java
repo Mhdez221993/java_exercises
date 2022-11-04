cbb809d3383a5e6ced115228a2926fe6b6d5e83b
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-04 19:03:42 +0100

    Write a program that print all ASCII characters

--- src/Programs/BasicPrograms/HowToPrintASCIIValue.java
+++ src/Programs/BasicPrograms/HowToPrintASCIIValue.java
@@ -4,13 +4,7 @@ import java.util.Scanner;
 
 public class HowToPrintASCIIValue {
     public static void main(String[] args) {
-        System.out.print("Enter a character: ");
-        Scanner in = new Scanner(System.in);
-
-        char chr = in.next().charAt(0);
-
-        int asciiValue = (int) chr;
-
-        System.out.println("ASCII value of " +chr+ " is: "+asciiValue);
+        for (int i = 0; i <= 225; i++)
+            System.out.println("The ASCII value of: " + (char)i + " = " + i);
     }
 }
