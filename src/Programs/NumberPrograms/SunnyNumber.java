5c35add53659d35cccd20aa707b84499e0165518
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-10 19:59:45 +0100

    Improve Synny program

--- src/Programs/NumberPrograms/SunnyNumber.java
+++ src/Programs/NumberPrograms/SunnyNumber.java
@@ -10,24 +10,30 @@ import java.util.Scanner;
 public class SunnyNumber {
     static boolean isSunny(int n) {
         // Find the square root of num+1.
-        double sqrt = (int)Math.sqrt(n);
-
         // If the square root is an integer, the given number is sunny, else not a sunny number.
-        return  (sqrt - Math.floor(sqrt) == 0);
+        if (Math.sqrt(n+1) % 1 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Introduce a number: ");
-
         // Read or initialize a number (num).
-        int n = in.nextInt();
+        System.out.print("Introduce the first number: ");
+        int from = in.nextInt();
 
-        // Add 1 to the given number i.e. num+
-        if (isSunny(n+1))
-            System.out.println("The number "+n+" is Sunny");
-        else
-            System.out.println("The number "+n+" is not Sunny");
+        System.out.print("Introduce the second number: ");
+        int to = in.nextInt();
+
+        for (int i = from; i <= to; i++) {
+            // Add 1 to the given number i.e. num+
+            if (isSunny(i))
+                System.out.println(i + " ");
+        }
+
     }
 }
