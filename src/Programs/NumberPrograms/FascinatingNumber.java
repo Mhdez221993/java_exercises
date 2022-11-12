1fdf2e3030f2beb4e89abc1effc8a2869299118a
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-12 11:14:47 +0100

    Define a fascinating number

---
+++ src/Programs/NumberPrograms/FascinatingNumber.java
@@ -0,0 +1,18 @@
+package Programs.NumberPrograms;
+
+/*
+Multiplying a number by two and three separately, the number obtained by writing the results obtained with the given number will be called a fascinating number.
+If the result obtained after concatenation contains all digits from 1 to 9, exactly once.
+
+In other words, we can also say that a number (n) may be a fascinating number if it satisfies the following two conditions:
+
+    If the given number is a 3 or more than three-digit
+
+    If the value getting after concatenation contains all digits from 1 to 9, exactly once.
+ */
+
+public class FascinatingNumber {
+    public static void main(String[] args) {
+
+    }
+}
