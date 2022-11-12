b469a5a9f6959b7998547cf35469b5d48fbb2004
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-12 11:53:55 +0100

    Write a program that check if a number is fascinating

--- src/Programs/NumberPrograms/FascinatingNumber.java
+++ src/Programs/NumberPrograms/FascinatingNumber.java
@@ -11,8 +11,52 @@ In other words, we can also say that a number (n) may be a fascinating number if
     If the value getting after concatenation contains all digits from 1 to 9, exactly once.
  */
 
-public class FascinatingNumber {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class FascinatingNumber {
+    static boolean isFascinating(int number) {
+        int n2 = number * 2;
+        int n3 = number * 3;
+
+        // concatenating num, n2, and n3
+        String concatNum = number + "" + n2 + n3;
+        boolean found = true;
+
+        // checks all digits from 1 to 9 are present or not
+        for (char c = '1'; c <= '9'; c++) {
+            int count = 0;
+
+            // loop counts the frequency of each digit
+            for (int i = 0; i < concatNum.length(); i++) {
+                char ch = concatNum.charAt(i);
+
+                // compares the character of concatNum with i
+                if (ch == c)
+                    count++;
+            }
+
+            // returns true if any of the condition returns true
+            if (count > 1 || count == 0) {
+                found = false;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Introduce a number to check: ");
+
+        int n = in.nextInt();
+
+        if (isFascinating(n))
+            System.out.println(n + " is fascinating");
+        else
+            System.out.println(n + " is not fascinating");
+        // Print "not a fascinating number" if any digit is missing or appeared multiple times. Else, print "fascinating number".
     }
 }
