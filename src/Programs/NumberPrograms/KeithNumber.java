834e4c27c52dccd493f62b1f31f3c1d75887c8b9
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-12 12:21:31 +0100

    Define a Keith number

---
+++ src/Programs/NumberPrograms/KeithNumber.java
@@ -0,0 +1,13 @@
+package Programs.NumberPrograms;
+
+/*
+A positive n digit number X is called a Keith number (or repfigit number) if it is arranged in a special number sequence generated using its digits.
+The special sequence has first n terms as digits of x and other terms are recursively evaluated as the sum of previous n terms.
+For example, 197, 19, 742, 1537, etc.
+ */
+
+public class KeithNumber {
+    public static void main(String[] args) {
+
+    }
+}
