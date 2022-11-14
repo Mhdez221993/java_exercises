a03bb639a5efd2da2f1ccdfee340234cacb0e2ae
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-14 22:31:25 +0100

    Define what a neon number is

---
+++ src/Programs/NumberPrograms/NeonNumber.java
@@ -0,0 +1,10 @@
+package Programs.NumberPrograms;
+
+/*
+A positive integer whose sum of digits of its square is equal to the number itself is called a neon number.
+ */
+public class NeonNumber {
+    public static void main(String[] args) {
+
+    }
+}
