5e8042aa58eec67dc307b12421b91ffa520d1e85
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-14 23:04:30 +0100

    Write a program to check if a number is Spy

--- src/Programs/NumberPrograms/SpyNumber.java
+++ src/Programs/NumberPrograms/SpyNumber.java
@@ -5,8 +5,32 @@ A positive integer is called a spy number if the sum and product of its digits a
 In other words, a number whose sum and product of all digits are equal is called a spy number.
  */
 
-public class SpyNumber {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class SpyNumber {
+    static boolean isSpy(int n) {
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+            int remainder = n % 10;
+            sum += remainder;
+            product *= remainder;
+            n /= 10;
+        }
+
+        return  (sum == product);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+
+        int n = in.nextInt();
+
+        if (isSpy(n))
+            System.out.println(n + " is spy");
+        else
+            System.out.println(n + " is not spy");
     }
 }
