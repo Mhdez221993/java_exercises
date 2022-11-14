48d2ab5607510005056077bac92199449eaee6b4
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-14 22:59:06 +0100

    Define what a spy number is

---
+++ src/Programs/NumberPrograms/SpyNumber.java
@@ -0,0 +1,12 @@
+package Programs.NumberPrograms;
+
+/*
+A positive integer is called a spy number if the sum and product of its digits are equal.
+In other words, a number whose sum and product of all digits are equal is called a spy number.
+ */
+
+public class SpyNumber {
+    public static void main(String[] args) {
+
+    }
+}
