3582f043284b89e160d19adcc16ceb23c97844b1
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-16 21:33:34 +0100

    Explain what a Sphenic number is

---
+++ src/Programs/NumberPrograms/SphenicNumber.java
@@ -0,0 +1,16 @@
+package Programs.NumberPrograms;
+
+/*
+A positive integer n is called a sphenic number
+if the product of factors of the given number (n)
+is exactly three
+and all factors are prime.
+
+42 is a sphenic number because it has exactly three prime factors 2, 3, and 7 and the product of these factors gives the number itself.
+ */
+
+public class SphenicNumber {
+    public static void main(String[] args) {
+
+    }
+}
