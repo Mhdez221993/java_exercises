bacfc75263b0ecb309e905d81ae13d615ed024d4
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-16 23:27:48 +0100

    Define what a Buzz number is

---
+++ src/Programs/NumberPrograms/BuzzNumber.java
@@ -0,0 +1,13 @@
+package Programs.NumberPrograms;
+
+/*
+Buzz number is another special number in Java
+that ends with digit 7 or
+divisible by 7.
+Unlike Prime and Armstrong numbers, the Buzz number is not so popular and asked by the interviewers.
+ */
+
+public class BuzzNumber {
+    public static void main(String[] args) {
+    }
+}
