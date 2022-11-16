2ae7a1547b31a50a2ce00ad65385b51e83944ad7
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-17 00:00:20 +0100

    Define evil number

---
+++ src/Programs/NumberPrograms/EvilNumber.java
@@ -0,0 +1,12 @@
+package Programs.NumberPrograms;
+
+/*
+The Evil number is another special positive whole number in Java that has an even number of 1's in its binary equivalent.
+Unlike Prime and Armstrong numbers, Evil number is not so popular and asked by the interviewers.
+ */
+
+public class EvilNumber {
+    public static void main(String[] args) {
+
+    }
+}
