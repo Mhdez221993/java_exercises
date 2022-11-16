bb36edbaa198dde954f177c17b9d9e04e4eef0e4
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-16 22:23:35 +0100

    Complete isSphenic program

--- src/Programs/NumberPrograms/SphenicNumber.java
+++ src/Programs/NumberPrograms/SphenicNumber.java
@@ -9,18 +9,47 @@ and all factors are prime.
 42 is a sphenic number because it has exactly three prime factors 2, 3, and 7 and the product of these factors gives the number itself.
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SphenicNumber {
+    static ArrayList<Integer> getFactorsOfN(int n) {
+        // hold the factors of n
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                arr.add(i);
+        }
+
+        return  arr;
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i <= (n/2); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
     static boolean isSphenic(int n) {
         // get all factors of n
-        int factorsOfN[] = getFactorsOfN(n);
+        ArrayList<Integer> factorsOfN = getFactorsOfN(n);
+
+        // the factors of n should be 8
+        if (factorsOfN.size() != 8)
+            return false;
 
         // count how many prime holds the factors array
         int counter = 0;
 
-        for (int i = 0; i < factorsOfN.length; i++) {
-            if (isPrime(factorsOfN[i]))
+        for (int i = 0; i < factorsOfN.size(); i++) {
+            if (isPrime(factorsOfN.get(i)))
                 counter++;
         }
 
