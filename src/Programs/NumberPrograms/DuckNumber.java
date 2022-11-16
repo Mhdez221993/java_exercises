012d495f4f08ab927b40537c45501ed27bc956d7
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-16 23:49:08 +0100

    Complete isBuzz program

--- src/Programs/NumberPrograms/DuckNumber.java
+++ src/Programs/NumberPrograms/DuckNumber.java
@@ -6,8 +6,30 @@ The digit zero should not be presented at the starting of the number.
 Zero can be present at any of the positions except the beginning of the number.
  */
 
-public class DuckNumber {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class DuckNumber {
+    static boolean isDuck(int n) {
+
+        while (n > 0) {
+            if (n % 10 == 0)
+                return true;
+
+            n /= 10;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+
+        int n = in.nextInt();
+
+        if (isDuck(n))
+            System.out.println("Is Duck");
+        else
+            System.out.println("Is not Duck");
     }
 }
