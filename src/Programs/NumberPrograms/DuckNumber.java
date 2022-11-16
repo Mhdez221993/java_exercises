33110357267e43a10bfaf907b2cc3e3eec2ee9a6
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-16 23:40:12 +0100

    Define what a Duck number is

---
+++ src/Programs/NumberPrograms/DuckNumber.java
@@ -0,0 +1,13 @@
+package Programs.NumberPrograms;
+
+/*
+Duck number is another special positive non-zero number that contains zero in it.
+The digit zero should not be presented at the starting of the number.
+Zero can be present at any of the positions except the beginning of the number.
+ */
+
+public class DuckNumber {
+    public static void main(String[] args) {
+
+    }
+}
